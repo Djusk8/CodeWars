919dc78c491afcee74899a40195859c786d05d07
SEO <djusk8@gmail.com>
2019-11-21 10:33:27 +0300

    Reformat "6 kyu - Find the odd int" solution

--- 6 kyu/Find the odd int.py
+++ 6 kyu/Find the odd int.py
@@ -1,14 +1,23 @@
-def find_it(seq):
-    items = set(seq)
-    for item in items:
-        cnt = items.count(item)
+# ------------  KATA DESCRIPTION ------------
+"""
+6 kyu - Find the odd int
 
-    return None
+Given an array, find the int that appears an odd number of times.
 
+There will always be only one integer that appears an odd number of times.
 
+"""
 
+# ---------------  SOLUTION ---------------
 import codewars_test as test
 
+
+def find_it(seq):
+    for item in set(seq):
+        if seq.count(item) % 2 == 1:
+            return item
+
+
+# ---------------  TEST CASES ---------------
 test.describe("Example")
 test.assert_equals(find_it([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5]), 5)
-
