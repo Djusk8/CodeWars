f690dc490695e6092774fe70155ff72d92e57049
SEO <djusk8@gmail.com>
2020-05-01 09:31:04 +0300

    Solved: 8 kyu - SpeedCode #2 - Array Madness

---
+++ 8 kyu/SpeedCode #2 - Array Madness.py
@@ -0,0 +1,24 @@
+# ------------  KATA DESCRIPTION ------------
+"""
+8 kyu - SpeedCode #2 - Array Madness
+
+SpeedCode #2 - Array Madness
+Objective
+Given two integer arrays a, b, both of length >= 1, create a program that returns true if the sum of the squares of
+each element in a is strictly greater than the sum of the cubes of each element in b.
+E.g.
+array_madness([4, 5, 6], [1, 2, 3]) => True #because 4 ** 2 + 5 ** 2 + 6 ** 2 > 1 ** 3 + 2 ** 3 + 3 ** 3
+Get your timer out.  Are you ready?  Ready, get set, GO!!!
+"""
+
+# ---------------  SOLUTION ---------------
+import codewars_test as test
+
+
+def array_madness(a, b):
+    return sum(map(lambda x: x**2, a)) > sum(map(lambda y: y**3, b))
+
+# ---------------  TEST CASES ---------------
+test.assert_equals(array_madness([4, 5, 6], [1, 2, 3]),True)
+test.assert_equals(array_madness( [1, 2, 3],[4, 5, 6]),False)
+
